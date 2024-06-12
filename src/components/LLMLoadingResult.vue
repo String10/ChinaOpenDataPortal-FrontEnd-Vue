45662f7b5951455493e940f9564965b1a6cf8e38
stringmax <stringcom@outlook.com>
2024-06-12 10:20:39 +0800

    loading view for LLM rerank

---
+++ src/components/LLMLoadingResult.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="empty">
+    <div class="empty-img">
+      <img src="@/assets/illustrations/undraw_quitting_time_dm8t.svg" height="128" alt="" />
+    </div>
+    <p class="empty-title">正在搜索合适的结果<span class="animated-dots"></span></p>
+    <p class="empty-subtitle text-secondary">大语言模型耗时较长，请耐心等待</p>
+  </div>
+</template>
